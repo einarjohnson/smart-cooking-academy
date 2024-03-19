d830f46a0dacdd33b61e0dc14124387131f4e839
Einar Johnson <einar.johnson@controlant.com>
2024-03-19 13:33:17 +0000

    test github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astroship.web3templates.com",
+  site: "https://einarjohnson.github.io",
+  base: '/',
   integrations: [tailwind(), mdx(), sitemap()],
 });

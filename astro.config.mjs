b77676ae4fa172ec784e7dbacddc5ff8bcdad087
Einar Johnson <einar.johnson@controlant.com>
2024-04-03 10:33:07 +0000

    fix paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://einarjohnson.github.io",
-  base: "/smart-cooking-academy",
+  base: "/",
   integrations: [tailwind(), mdx(), sitemap()],
 });

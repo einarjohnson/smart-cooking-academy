cc29b5bc6f9ffe3eeca9dc7d2d9cbccffc19cb9b
dusky <dusky@duskys-MacBook-Air.local>
2026-01-29 02:36:29 -0800

    Remove sitemap integration to fix build error

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://einarjohnson.github.io",
   base: "/smart-cooking-academy/",
-  integrations: [tailwind(), mdx(), sitemap()],
+  integrations: [tailwind(), mdx()],
 });
